bd7dbb129abcc76d29ed905f01d132a4f6a63a7a
Nathan Rassi <nrassi@matthew.cse.taylor.edu>
2014-04-02 13:31:36 -0400

    Nathan Rassi

---
+++ TrojanTrainer/Logout.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class Logout
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnloginpage_Click(sender As Object, e As System.EventArgs) Handles btnloginpage.Click
+        Response.Redirect("Login.aspx")
+    End Sub
+End Class
