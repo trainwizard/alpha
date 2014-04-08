eb645a3cd039ed7b4410960f31b18182acd1d0cf
trainwizard <samuel_bolds@taylor.edu>
2014-04-08 13:51:28 -0400

    sbolds

---
+++ TrojanTrainer/Workouts.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class Workouts
+    Inherits System.Web.UI.Page
+
+    Protected Sub ListView1_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles ListView1.SelectedIndexChanged
+
+    End Sub
+End Class
