b861de858c37f4e175a9d6ed955992a1d4fcb74f
Devin Moore <dmoore@matthew.cse.taylor.edu>
2014-04-10 21:46:06 -0400

    Devin

--- TrojanTrainer/Roster.aspx.vb
+++ TrojanTrainer/Roster.aspx.vb
@@ -5,16 +5,28 @@ Imports System.Diagnostics
 Partial Class Roster
     Inherits System.Web.UI.Page
 
-    Protected Sub ddlSports_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles ddlSports.SelectedIndexChanged
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        'ddlTeams.Enabled = False
+        'btnSubmit.Visible = False
+        'gvRoster.EnableViewState = False
+        gvRoster.Visible = False
         ddlTeams.Enabled = True
+    End Sub
+
+    Protected Sub ddlSports_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles ddlSports.SelectedIndexChanged
+
         'ddlTeams.Items.Insert(0, New ListItem("All", -1))
+
     End Sub
 
     Protected Sub ddlTeams_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles ddlTeams.SelectedIndexChanged
-        gvTeamRoster.Enabled = True
+        'btnSubmit.Visible = True
     End Sub
 
-    Protected Sub btnShowAll_Click(sender As Object, e As System.EventArgs) Handles btnShowAll.Click
-        gvSportRoster.Enabled = True
+    Protected Sub btnSubmit_Click(sender As Object, e As System.EventArgs) Handles btnSubmit.Click
+        '    'gvRoster.EnableViewState = True
+        gvRoster.Visible = True
     End Sub
+
+
 End Class
