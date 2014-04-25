12ecd3cd488c14400fa4b43894daa28f9f3f57f9
Nathan Rassi <nrassi@matthew.cse.taylor.edu>
2014-04-24 22:14:43 -0400

    Nathan Rassi

--- TrojanTrainer/Athlete_Sleep_Nutrition.aspx.vb
+++ TrojanTrainer/Athlete_Sleep_Nutrition.aspx.vb
@@ -3,13 +3,11 @@ Imports System.Data
 Imports System.Diagnostics
 
 Partial Class Athlete_Sleep_Nutrition
-    'Inherits System.Web.UI.Page
+    Inherits System.Web.UI.Page
     'Dim connectionString As String = "Data Source=SIMON;Initial Catalog=AlphaSYS39414;Persist Security Info=True;User ID=sbolds;Password=ttpfrzeh"
 
     'Protected Sub Submit_Sleep_Click(sender As Object, e As System.EventArgs) Handles Submit_Sleep.Click
 
-
-    End Sub
 
     Protected Sub SubmitNutrition_Click(sender As Object, e As System.EventArgs) Handles SubmitNutrition.Click
 
