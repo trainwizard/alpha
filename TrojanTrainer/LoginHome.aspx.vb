550a54674e9ed9ff944f3114cf85d4ebca94e32f
trainwizard <samuel_bolds@taylor.edu>
2014-04-28 13:51:02 -0400

    sbolds

--- TrojanTrainer/LoginHome.aspx.vb
+++ TrojanTrainer/LoginHome.aspx.vb
@@ -5,11 +5,6 @@ Partial Class LoginHome
     Inherits System.Web.UI.Page
     Dim connectionString As String = "Data Source=SIMON;Initial Catalog=AlphaSYS39414;Persist Security Info=True;User ID=sbolds;Password=ttpfrzeh"
 
-    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
-        UserImage.ImageUrl = Session.Item("UserPicture")
-    End Sub
 
-    Protected Sub UserImage_Click(sender As Object, e As System.Web.UI.ImageClickEventArgs) Handles UserImage.Click
 
-    End Sub
 End Class
