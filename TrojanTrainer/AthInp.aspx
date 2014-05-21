bbbbb9331320fabb10f6ab5800f497e484845a44
Nathan Rassi <nrassi@matthew.cse.taylor.edu>
2014-05-20 21:11:16 -0400

    Nathan Rassi

--- TrojanTrainer/AthInp.aspx
+++ TrojanTrainer/AthInp.aspx
@@ -36,6 +36,8 @@
         </asp:SqlDataSource>
     </p>
     <p>
+        <strong>Exercise Recommendations:</strong></p>
+    <p>
         <asp:FormView ID="FormView1" runat="server" DataSourceID="SqlDataSource3">
             <EditItemTemplate>
                 Name:
@@ -119,67 +121,48 @@
         </asp:FormView>
     </p>
     <p>
-        <asp:DetailsView ID="DetailsView1" runat="server" AutoGenerateRows="False" 
-            DataKeyNames="Actual_Ex_ID" DataSourceID="SqlDataSource4" Height="50px" 
-            Width="125px">
-            <Fields>
-                <asp:BoundField DataField="Reps" HeaderText="Reps" 
-                    SortExpression="Reps" />
-                <asp:BoundField DataField="Sets" HeaderText="Sets" SortExpression="Sets" />
-                <asp:BoundField DataField="Weight" HeaderText="Weight" 
-                    SortExpression="Weight" />
-                <asp:BoundField DataField="Time" HeaderText="Time" SortExpression="Time" />
-                <asp:BoundField DataField="Intensity" HeaderText="Intensity" 
-                    SortExpression="Intensity" />
-                <asp:BoundField DataField="Note" HeaderText="Note" SortExpression="Note" />
-            </Fields>
-            <EmptyDataTemplate>
-                <asp:Button ID="InsertButton" runat="server" CommandName="New" Text="Complete Exercise" />
-            </EmptyDataTemplate>
-        </asp:DetailsView>
-
-    </p>
+        <strong>Results:</strong></p>
     <p>
-        <asp:Label ID="RepsLbl" runat="server" Text="Reps:"></asp:Label>
+        <asp:Label ID="RepsLbl" runat="server" Text="Reps:" Visible="False"></asp:Label>
    
-<asp:TextBox ID="RepsTxt" runat="server"></asp:TextBox>
+<asp:TextBox ID="RepsTxt" runat="server" Visible="False"></asp:TextBox>
 
         </p>
 
     <p>
-        <asp:Label ID="SetsLbl" runat="server" Text="Sets:"></asp:Label>
+        <asp:Label ID="SetsLbl" runat="server" Text="Sets:" Visible="False"></asp:Label>
     </p>
     <p>
    
-<asp:TextBox ID="SetsTxt" runat="server"></asp:TextBox>
+<asp:TextBox ID="SetsTxt" runat="server" Visible="False"></asp:TextBox>
 
-        <asp:Label ID="WeightLbl" runat="server" Text="Weight:"></asp:Label>
+        <asp:Label ID="WeightLbl" runat="server" Text="Weight:" Visible="False"></asp:Label>
     </p>
     <p>
    
-<asp:TextBox ID="WeightTxt" runat="server"></asp:TextBox>
+<asp:TextBox ID="WeightTxt" runat="server" Visible="False"></asp:TextBox>
 
-        <asp:Label ID="TimeLbl" runat="server" Text="Time:"></asp:Label>
+        <asp:Label ID="TimeLbl" runat="server" Text="Time:" Visible="False"></asp:Label>
     </p>
     <p>
    
-<asp:TextBox ID="TimeTxt" runat="server"></asp:TextBox>
-
-    </p>
-    <p>
-        <asp:Label ID="IntensityLbl" runat="server" Text="Intensity:"></asp:Label>
-    </p>
-    <p>
-   
-<asp:TextBox ID="IntensityTxt" runat="server"></asp:TextBox>
+<asp:TextBox ID="TimeTxt" runat="server" Visible="False"></asp:TextBox>
 
     </p>
     <p>
-        <asp:Label ID="NoteLbl" runat="server" Text="Note:"></asp:Label>
+        <asp:Label ID="IntensityLbl" runat="server" Text="Intensity:" Visible="False"></asp:Label>
     </p>
     <p>
    
-<asp:TextBox ID="NoteTxt" runat="server"></asp:TextBox>
+<asp:TextBox ID="IntensityTxt" runat="server" Visible="False"></asp:TextBox>
+
+    </p>
+    <p>
+        <asp:Label ID="NoteLbl" runat="server" Text="Note:" Visible="False"></asp:Label>
+    </p>
+    <p>
+   
+<asp:TextBox ID="NoteTxt" runat="server" Visible="False"></asp:TextBox>
 
     </p>
     <p>
@@ -187,29 +170,7 @@
             Text="Add Exercise Results" />
     </p>
     <p>
-        &nbsp;</p>
-    <p>
-
-        <asp:SqlDataSource ID="SqlDataSource4" runat="server" 
-            ConnectionString="<%$ ConnectionStrings:AlphaConnectionString %>" 
-            InsertCommand="AthleteInputWorkoutData" InsertCommandType="StoredProcedure" SelectCommand="SELECT *
-FROM Actual_Exercise
-WHERE TM_ID = @TM_ID AND Date = @Date">
-            <InsertParameters>
-                <asp:Parameter Name="TM_ID" Type="Int32" />
-                <asp:Parameter DbType="Date" Name="Date" />
-                <asp:Parameter Name="Reps" Type="Int32" />
-                <asp:Parameter Name="Sets" Type="Int32" />
-                <asp:Parameter Name="Weight" Type="Double" />
-                <asp:Parameter Name="Time" Type="String" />
-                <asp:Parameter Name="Intensity" Type="Double" />
-                <asp:Parameter Name="Note" Type="String" />
-            </InsertParameters>
-            <SelectParameters>
-                <asp:SessionParameter Name="TM_ID" SessionField="TM_ID" />
-                <asp:SessionParameter Name="Date" SessionField="CurDate" />
-            </SelectParameters>
-        </asp:SqlDataSource>
+        <asp:Label ID="VerificationLbl" runat="server" Text="Label" Visible="False"></asp:Label>
     </p>
     <p>
         <asp:SqlDataSource ID="SqlDataSource3" runat="server" 
--- TrojanTrainer/AthInp.aspx.vb
+++ TrojanTrainer/AthInp.aspx.vb
@@ -1,21 +1,130 @@
 ﻿Imports System.Diagnostics
+Imports System.Data.SqlClient
+Imports System.Data
 
 Partial Class _Default
     Inherits System.Web.UI.Page
+    Dim connectionString As String = "Data Source=SIMON;Initial Catalog=AlphaSYS39414;Persist Security Info=True;User ID=sbolds;Password=ttpfrzeh"
 
     Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
         Dim WorkoutID As String = Request.QueryString("ID")
-        Debug.Print(Session.Item("TM_ID"))
-        Debug.Print((DetailsView1.Rows.Count))
         'If DetailsView1.Rows.Count > 1 Then
         '    DetailsView1.ChangeMode(DetailsViewMode.ReadOnly)
         '    DetailsView1.AutoGenerateInsertButton = False
         'Else
         '    DetailsView1.ChangeMode(DetailsViewMode.Insert)
         'End If
+        RepsLbl.Visible = True
+        RepsTxt.Visible = True
+        SetsLbl.Visible = True
+        SetsTxt.Visible = True
+        WeightLbl.Visible = True
+        WeightTxt.Visible = True
+        TimeLbl.Visible = True
+        TimeTxt.Visible = True
+        NoteLbl.Visible = True
+        NoteTxt.Visible = True
+        AddExerciseDataBtn.Visible = True
+
     End Sub
 
     Protected Sub ExDdl_SelectedIndexChanged(sender As Object, e As System.EventArgs) Handles ExDdl.SelectedIndexChanged
-        CurrentExDv.DataBind()
+        Using connection As New SqlConnection(connectionString)
+            'connection is established
+            connection.Open()
+            Dim Exreader As SqlDataReader
+            'checks for returned user if true prints error message
+            Dim Viewcmd As SqlCommand = New SqlCommand("ViewExerciseResults", connection)
+            Viewcmd.CommandType = CommandType.StoredProcedure
+            Viewcmd.Parameters.Add(New SqlParameter("@TM_ID", Session.Item("TM_ID")))
+            Viewcmd.Parameters.Add(New SqlParameter("@Date", Session.Item("CurDate")))
+            Viewcmd.Parameters.Add(New SqlParameter("@Planned_Ex_ID", ExDdl.SelectedValue))
+            Exreader = Viewcmd.ExecuteReader()
+            If Exreader.HasRows() Then
+                Do While Exreader.Read()
+                    If Str(Exreader("Reps")) <> "" Then
+                        RepsLbl.Visible = True
+                        SetsTxt.Visible = True
+                        RepsTxt.Text = Exreader("Reps")
+                        RepsTxt.ReadOnly = True
+                    End If
+                    If Str(Exreader("Sets")) <> "" Then
+                        SetsLbl.Visible = True
+                        SetsTxt.Visible = True
+                        SetsTxt.Text = Exreader("Sets")
+                        SetsTxt.ReadOnly = True
+                    End If
+                    If Str(Exreader("Weight")) <> "" Then
+                        WeightLbl.Visible = True
+                        WeightTxt.Visible = True
+                        WeightTxt.Text = Exreader("Weight")
+                        WeightTxt.ReadOnly = True
+                    End If
+                    Debug.Print("Time:" + Exreader("Time") + ".")
+                    Dim time As String = " "
+                    Dim TimeActual As String = Exreader("Time")
+                    If time <> TimeActual Then
+                        TimeLbl.Visible = True
+                        TimeTxt.Visible = True
+                        TimeTxt.Text = Exreader("Time")
+                        TimeTxt.ReadOnly = True
+                    End If
+                    If Exreader("Note") <> "" Then
+                        NoteLbl.Visible = True
+                        NoteTxt.Visible = True
+                        NoteTxt.Text = Exreader("Note")
+                        NoteTxt.ReadOnly = True
+                        NoteTxt.TextMode = TextBoxMode.MultiLine
+                    End If
+                Loop
+                AddExerciseDataBtn.Visible = False
+            Else
+                RepsLbl.Visible = True
+                RepsTxt.Visible = True
+                SetsLbl.Visible = True
+                SetsTxt.Visible = True
+                WeightLbl.Visible = True
+                WeightTxt.Visible = True
+                TimeLbl.Visible = True
+                TimeTxt.Visible = True
+                NoteLbl.Visible = True
+                NoteTxt.Visible = True
+                AddExerciseDataBtn.Visible = True
+            End If
+
+        End Using
+    End Sub
+
+    Protected Sub AddExerciseDataBtn_Click(sender As Object, e As System.EventArgs) Handles AddExerciseDataBtn.Click
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+            Dim insrtcmd As SqlCommand = New SqlCommand("AthleteInputWorkoutData", connection)
+            insrtcmd.CommandType = CommandType.StoredProcedure
+            insrtcmd.Parameters.Add(New SqlParameter("@TM_ID", Session.Item("TM_ID")))
+            insrtcmd.Parameters.Add(New SqlParameter("@Date", Session.Item("CurDate")))
+            insrtcmd.Parameters.Add(New SqlParameter("@Reps", RepsTxt.Text))
+            insrtcmd.Parameters.Add(New SqlParameter("@Sets", SetsTxt.Text))
+            insrtcmd.Parameters.Add(New SqlParameter("@Weight", WeightTxt.Text))
+            insrtcmd.Parameters.Add(New SqlParameter("@Time", TimeTxt.Text))
+            insrtcmd.Parameters.Add(New SqlParameter("@Note", NoteTxt.Text))
+            insrtcmd.Parameters.Add(New SqlParameter("@Intensity", ""))
+            insrtcmd.Parameters.Add(New SqlParameter("@Planned_Ex_ID", ExDdl.SelectedValue))
+            insrtcmd.ExecuteNonQuery()
+            connection.Close()
+            VerificationLbl.Text = "Exercise has been submitted"
+            VerificationLbl.ForeColor = Drawing.Color.Green
+            VerificationLbl.Visible = True
+            RepsLbl.Visible = False
+            RepsTxt.Visible = False
+            SetsLbl.Visible = False
+            SetsTxt.Visible = False
+            WeightLbl.Visible = False
+            WeightTxt.Visible = False
+            TimeLbl.Visible = False
+            TimeTxt.Visible = False
+            NoteLbl.Visible = False
+            NoteTxt.Visible = False
+            AddExerciseDataBtn.Visible = False
+        End Using
     End Sub
 End Class
